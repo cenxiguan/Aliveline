442e2039594cb475912f7c8d1db9a5102c22d41b
jmaeda <thecopyholder@gmail.com>
2015-05-31 01:05:00 -0400

    create basic popup

--- app/src/main/java/cajac/aliveline/MainActivity.java
+++ app/src/main/java/cajac/aliveline/MainActivity.java
@@ -1,9 +1,12 @@
 package cajac.aliveline;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.addTodo);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,addTodo.class));
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/cajac/aliveline/addTodo.java
@@ -0,0 +1,24 @@
+package cajac.aliveline;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Jonathan Maeda on 5/31/2015.
+ */
+public class addTodo extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addtodo);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.8));
+    }
+}
