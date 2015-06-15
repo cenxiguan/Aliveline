3f9bc4abe32a197c94a54dd64154fe65c4d49949
amfer17 <amfer17@brandeis.edu>
2015-06-14 20:57:59 -0400

    add card views

--- app/src/main/java/cajac/aliveline/CardArrayAdapter.java
+++ app/src/main/java/cajac/aliveline/CardArrayAdapter.java
@@ -1,7 +1,41 @@
 package cajac.aliveline;
 
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Amauris on 6/14/2015.
  */
-public class CardArrayAdapter {
+public class CardArrayAdapter extends ArrayAdapter<Todo> {
+    List<Todo> todoCards;
+
+    public CardArrayAdapter(Context context, int resource, int textViewResourceId, List<Todo> todoCards){
+        super(context, resource, textViewResourceId, todoCards);
+        this.todoCards = todoCards;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View v = convertView;
+        if (v == null){
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.card_view_list, null);
+        }
+        Todo todo = todoCards.get(position);
+
+        CardView cardView = (CardView)v.findViewById(R.id.card_view);
+        TextView tv = (TextView)cardView.findViewById(R.id.card_header);
+        TextView desc = (TextView)cardView.findViewById(R.id.desc);
+        desc.setText(todo.getDescription());
+        tv.setText(todo.getTitle());
+
+        return v;
+    }
+
 }
